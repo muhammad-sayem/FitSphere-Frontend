185d12a796be9e5587a866631912baf2dab34a1b
muhammad-sayem <shahrulislamsayem@gmail.com>
2026-05-27 16:32:55 +0600

    Basic Setup

---
+++ src/app/(commonLayout)/about-us/page.tsx
@@ -0,0 +1,9 @@
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">About Us</h1>
+    </div>
+  );
+};
+
+export default AboutUsPage;
---
+++ src/app/(commonLayout)/contact-us/page.tsx
@@ -0,0 +1,9 @@
+const ContactUsPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Contact Us</h1>
+    </div>
+  );
+};
+
+export default ContactUsPage;
